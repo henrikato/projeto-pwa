5b19e0d1b6facb2578a025dc9928c578456d1aa9
henrikato <henrique.kato2@outlook.com>
2020-02-17 20:47:11 -0300

    dedo no push e gritaria

--- precache-manifest.6ed06d743b79a63e029a605808600dfc.js
+++ precache-manifest.6ed06d743b79a63e029a605808600dfc.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "78aebd1b39532c0d53a883ed388a68ec",
-    "url": "//img/fav-192.png"
+    "url": "/img/fav-192.png"
   },
   {
     "revision": "5ca388ba2feff01dc58f1a54db89c9fd",
-    "url": "//img/fav.png"
+    "url": "/img/fav.png"
   },
   {
     "revision": "3b4d5f4652754b0a1b831b6a2fa2c8c1",
-    "url": "//img/icon-152.png"
+    "url": "/img/icon-152.png"
   },
   {
     "revision": "9d11c272f88e57258da57feeb1d906dc",
-    "url": "//img/icon-167.png"
+    "url": "/img/icon-167.png"
   },
   {
     "revision": "7b3145af8296262dc179f42e6f52695b",
-    "url": "//img/icon-180.png"
+    "url": "/img/icon-180.png"
   },
   {
     "revision": "3f72ccaf9650643e7993a14db650313a",
